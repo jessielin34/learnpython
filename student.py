b4804d90360f142224a24f5cb3694bc7be712d49
jessielin34 <80348079+jessielin34@users.noreply.github.com>
2024-06-19 11:52:35 -0400

    classes

--- student.py
+++ student.py
@@ -1,7 +1,22 @@
 class Student: 
     def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name") 
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
         self.name = name
         self.house = house
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+    def charm(self):
+        match self.patronus:
+            case "Stag":
+                return "Horse"
+            case "Otter":
+                return "Otter"
+            case _:
+                return "/"
+
 def main():
     name = get_name()
     house = get_house()
@@ -19,7 +34,11 @@ def get_house():
 def get_student():
     name = input("Name: ")
     house = input("House: ")
+    student = Student(name, house) 
     return (name, house)
+    #try:
+        #return Student(name,house)
+    #except Value:
 
 if __name__ == "__main__":
     main()
